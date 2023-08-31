c13eeb7460cddf0f426726513b1e77e193539501
Amresh Tripathy <amreshtripathy184@gmail.com>
2023-08-31 22:07:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+const calc = () => {
+	document.querySelector("h1")
+   .textContent = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`
+}
+
+window.addEventListener("resize", calc);
+calc()
